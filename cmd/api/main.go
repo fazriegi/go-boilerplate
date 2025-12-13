d83fb9c8f9e3a23f53d64edce4560b491655bc80
fazriegi <fazriegi80@gmail.com>
2025-12-13 18:20:39 +0700

    refactor: cors config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -23,14 +23,17 @@ func main() {
 
 	app := fiber.New()
 
+	origins := config.GetString("cors.origins")
+
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "*",
-		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
-		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+		AllowOrigins:     origins,
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowCredentials: true,
 	}))
 
 	app.Use(middleware.LogMiddleware())
-	port := viperConfig.GetInt("web.port")
+	port := config.GetInt("web.port")
 	router.NewRoute(app, jwt)
 
 	log.Fatal(app.Listen(fmt.Sprintf(":%d", port)))
